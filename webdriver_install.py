0137af1b380118041255d3fdd92f0b15d37dc41d
cringham <cringham@mail.umw.edu>
2019-03-24 14:29:22 -0400

    Delete webdriver_install.py

--- webdriver_install.py
+++
@@ -1,3 +0,0 @@
-import chromedriver_install as cdi
-path = cdi.install(file_directory='./lib/', verbose=True, chmod=True, overwrite=False, version=None, filename=None)
-print('Installed chromedriver to path: %s' % path)
