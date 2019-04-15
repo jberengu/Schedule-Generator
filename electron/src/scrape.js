32db4d6e5c9679b8a90a8bb9e30e5040e7ee86f7
Jose Berenguel-Araujo <jberengu@mail.umw.edu>
2019-04-15 12:27:57 -0400

    removed scrape.js and table.js

--- electron/src/scrape.js
+++
@@ -1,6 +0,0 @@
-var request = require("request");
-var cheerio = require("cheerio");
-
-
-windowObjectReference = window.open("http://some_site.html","name");
-windowObjectReference.forms[0].submit();
