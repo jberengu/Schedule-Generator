de27393e0f9b928b0ac44ea45ff5a4a9c4949b8d
Daniel <danielzamojda@gmail.com>
2019-04-16 13:49:24 -0400

    was able to delete professors from the database

--- electron/src/generateProfessorTable.js
+++ electron/src/generateProfessorTable.js
@@ -20,17 +20,18 @@ con.connect(function(err) {
         tableHTML = tableHTML.concat('<th>Phone</th>');
         tableHTML = tableHTML.concat('<th>Office</th>');
         tableHTML = tableHTML.concat('<th>Email</th>');
+        tableHTML = tableHTML.concat('<th>Delete Me?</th>');
         tableHTML = tableHTML.concat('</thead>');
         tableHTML = tableHTML.concat('<tbody contenteditable="true">');
 
-        console.log(profData)
-                // Make a <tr> for each class this prof has
         for (let i = 0; i < profData.length; i++) {
             tableHTML = tableHTML.concat('<tr>');
             tableHTML = tableHTML.concat('<td>' + profData[i].name + '</td>');
             tableHTML = tableHTML.concat('<td>' + profData[i].phone + '</td>');
             tableHTML = tableHTML.concat('<td>' + profData[i].office + '</td>');
             tableHTML = tableHTML.concat('<td>' + profData[i].email + '</td>');
+           // <input type="button" id="bt" value="Create Checkbox" onclick="createChk(prod)" />
+            tableHTML = tableHTML.concat("<td> <input class=deleteMe name=" + "'" + profData[i].name +"'" + 'type=button onclick=deleteThis(this) value=DELETE></td>')
             tableHTML = tableHTML.concat('</tr>');
         }
 
